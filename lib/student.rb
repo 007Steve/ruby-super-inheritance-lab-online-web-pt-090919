3d0ecf62692f20e413a1392f51e015429d24211f
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2019-10-18 05:48:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,8 +3,11 @@ require_relative "./chatty_student.rb"
 class Student 
   
   def hello
-    puts ""
+    puts "Hey there! I'm so excited to learn stuff."
   end
   
+  def raise_hand
+    puts "Pick me!"
+  end
   
 end
